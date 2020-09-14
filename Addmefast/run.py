c020135eca2f168ea9026b2f5307c861dfad6dca
Asım <asimzorlu@gmail.com>
2020-09-14 13:43:22 +0300

    Add files via upload

--- Addmefast/run.py
+++ Addmefast/run.py
@@ -2,8 +2,10 @@ import os
 import random
 
 array = ['python instaFollow.py', 'python instaLike.py', 'python YoutubeSubscribe.py',
-        'python tiktokLike.py', 'python tiktokFollow.py', 'telegram.py', 'soundcloudFollow.py', 'soundcloudLike.py']
+        'python tiktokLike.py', 'python tiktokFollow.py', 'python telegram.py',
+        'python soundcloudFollow.py', 'python soundcloudLike.py']
 
 for i in range(90):
     j = random.choice(array)
+    print(j)
     os.system(j)
